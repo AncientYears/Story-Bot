19602d55535eb3f36b404bc8644b1f1b3654bb1e
bailey-f <baileyeditor@gmail.com>
2019-06-26 17:04:00 +0100

    updated to make neater

--- commands/createstory.js
+++ commands/createstory.js
@@ -4,59 +4,54 @@ let storiesJSON = require('../stories.json')
 
 module.exports.run = async (client, message, args) => {
     if (!args[0]) { 
+    let userTitle, userPlot, userIntro, userClimax, userConclusion;
 
-    let csTitle = new discord.RichEmbed()
+    let filter = m => m.author.id == message.author.id;
+
+    let createStory = new discord.RichEmbed()
     .setAuthor('Create A New Title', client.user.displayAvatarURL)
     .addField('Create a new title for your story (make this brief and catchy!)')
     .setColor('GREEN')
-    message.channel.send(csTitle)
 
-    let filter = m => m.content.startsWith('?title');
-    message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected = new userTitle)
+    message.channel.send(createStory).then((msg) => {
 
-    let csPlot = new discord.RichEmbed()
-    .setAuthor('Create A New Plot', client.user.displayAvatarURL)
-    .addField('Write a new plot for your story (keep this brief!)')
-    .setColor('GREEN')
-    message.channel.send(csPlot)
+        message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+            .then(collected = userTitle),
 
-    filter = m => m.content.startsWith('?plot');
-    message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected = new userPlot)
+        msg.edit(
+            setAuthor('Create A New Plot', client.user.displayAvatarURL),
+            setDescription('Write a new plot for your story (bear in mind the character limit is 2000!)')),
+
+        message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+            .then(collected = userPlot),
+
+        msg.edit(
+            setAuthor('Create A New Introduction', client.user.displayAvatarURL),
+            setDescription('Write a new introduction to your story (bear in mind the character limit is 2000!)')),
+
+        message.channel.awaitMessages(filter, { max: 1, time: 6000, errors: ['time'] })
+            .then(collected = userIntro),
+
+        msg.edit(
+        setAuthor('Create A New Climax', client.user.displayAvatarURL),
+        setDescription('Write a new climax to your story (bear in mind the character limit is 2000!)')),
+
+        message.channel.awaitMessages(filter, { max: 1, time: 6000, errors: ['time'] })
+            .then(collected = userClimax),
+
+        msg.edit(
+            setAuthor('Create A New Conclusion', client.user.displayAvatarURL),
+            setDescription('Write a new conclusion to your story (bear in mind the character limit is 2000!)')),
+
+        message.channel.awaitMessages(filter, { max: 1, time: 6000, errors: ['time'] })
+            .then(collected = userConclusion)
     
-    let csIntro = new discord.RichEmbed()
-    .setAuthor('Create A New Introduction', client.user.displayAvatarURL)
-    .addField('Write a new introduction to your story (bear in mind the character limit is 2000!)')
-    .setColor('GREEN')
-    message.channel.send(csIntro)
-    
-    filter = m => m.content.startsWith('?intro');
-    message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected = new userIntro)
+    })
 
-    let csClimax = new discord.RichEmbed()
-    .setAuthor('Create A New Climax', client.user.displayAvatarURL)
-    .addField('Write a new climax/middle to your story (bear in mind the character limit is 2000!)')
-    .setColor('GREEN')
-    message.channel.send(csClimax)
-
-    filter = m => m.content.startsWith('?climax');
-    message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected = new userClimax)
-    
-    let csConclusion = new discord.RichEmbed()
-    .setAuthor('Create A New Conclusion', client.user.displayAvatarURL)
-    .addField('Write a new conclusion to your story (bear in mind the character limit is 2000!)')
-    .setColor('GREEN')
-    message.channel.send(csConclusion)
-
-    filter = m => m.content.startsWith('?conclusion');
-    message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected = new userConclusion)
 
     let createStoryEmbed = new discord.RichEmbed()
-    .setAuthor('Title: ', message.author.displayAvatarURL)    
+    .setAuthor('Author: ', message.author.displayAvatarURL) 
+    .addField('Title: ', userTitle)   
     .addField('Plot: ', '\n' + userPlot)
     .addField('Introduction: ', '\n' + userIntro)
     .addField('Climax: ', '\n'+ userClimax)
@@ -65,9 +60,6 @@ module.exports.run = async (client, message, args) => {
     .setColor('GREEN')
     message.channel.send(createStoryEmbed)
 
-
-
-       
 }
 }
 module.exports.help = {    
