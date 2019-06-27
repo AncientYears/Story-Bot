9ef71816893adb531d339afe08f622a41ba1f894
bailey-f <baileyeditor@gmail.com>
2019-06-27 16:04:29 +0100

    added help commands

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const discord = module.require("discord.js")
+
+let storiesJSON = require('../stories.json')
+
+module.exports.run = async (client, message, args) => {
+    if (!args[0]) { 
+    let helpEmbed = new discord.RichEmbed()
+    .setAuthor('Help', client.user.displayAvatarURL)    
+    .setColor('GREEN')
+    .addField('To view the stored stories, use: ', '?story')
+    .setDescription('Sorted by new (fetched from database)')
+    .addField('To create your own story, use: ', '?createstory')
+    .setDescription('The format is by pasting parts of the story in order,\nTitle, Plot, Introduction, Climax, Conclusion.')
+    .addField('To ping, use: ', '?ping')
+    .setDescription('Testing purposes.')
+
+    message.channel.send(helpEmbed)
+    }
+}
