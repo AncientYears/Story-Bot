b5e32aedc71873342d193153274cf93afaa94efe
bailey-f <baileyeditor@gmail.com>
2019-06-28 02:00:48 +0100

    fixed story viewer for client

--- commands/mystories.js
+++ commands/mystories.js
@@ -2,7 +2,6 @@ const discord = module.require("discord.js")
 
 let storiesJSON = require('../stories.json')
 
-storyStorage = []
 
 module.exports.run = async (client, message, args, pool) => {
     if (!args[0]) { 
@@ -10,24 +9,25 @@ module.exports.run = async (client, message, args, pool) => {
         myStoriesEmbed.setAuthor(message.author.username, message.author.displayAvatarURL)
         myStoriesEmbed.setColor('GREEN')
 
-        pool.query(`SELECT * FROM stories`, function (error, results, fields) {
+        pool.query(`SELECT * FROM stories WHERE id = '${message.author.id}'`, function (error, results, fields) {
             if(error) throw error;
+            let storyStorage = []
             for(i = 0; i < results.length; i++){
-                if(results[i].id == message.author.id){
-                    if(results[i].storyJSON){
-                        let storyObj = JSON.parse(results[i].storyJSON.split(","))
-                        for(i = 0; i < storyObj.length; i++) {
-                            storyStorage.push(storyObj[i])
-                        }
+                if(results[i].storyJSON){
+                    let storyObj = JSON.parse(results[i].storyJSON.split(","))
+                    for(i = 0; i < storyObj.length; i++) {
+                        console.log('4')
+                        storyStorage.push(storyObj[i])
                     }
                 }
+                
             }
             for(i = 0; i < storyStorage.length; i++){
                 myStoriesEmbed.addField(storyStorage[i].title, storyStorage[i].plot)
                 console.log(storyStorage[i])
             }
-    
             message.channel.send(myStoriesEmbed)
+            
         })
     }
 }
